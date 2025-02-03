905e56a0d74e9ac43c04e42e56f58f700e791f7d
Nguyen <huunguyen199999@gmail.com>
2025-02-03 11:18:10 +0700

    [Easy] Search Insert Position

---
+++ src/Easy/SearchInsertPosition.js
@@ -0,0 +1,42 @@
+/*
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+	Input: nums = [1,3,5,6], target = 5
+	Output: 2
+Example 2:
+	Input: nums = [1,3,5,6], target = 2
+	Output: 1
+Example 3:
+	Input: nums = [1,3,5,6], target = 7
+	Output: 4
+
+Constraints:
+	1 <= nums.length <= 104
+	-104 <= nums[i] <= 104
+	nums contains distinct values sorted in ascending order.
+	-104 <= target <= 104
+
+Topics: Array, Binary Search
+*/
+
+const searchInsert = (nums, target) => {
+	let left = 0;
+	let right = nums.length - 1;
+
+	while (left <= right) {
+		const mid = Math.floor((left + right) / 2);
+		if (nums[mid] === target) return mid;
+		if (nums[mid] < target) {
+			left = mid + 1;
+		} else {
+			right = mid - 1;
+		}
+	}
+	return left;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5));
+console.log(searchInsert([1, 3, 5, 6], 2));
+console.log(searchInsert([1, 3, 5, 6], 7));
