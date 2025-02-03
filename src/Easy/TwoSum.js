0bb77117365e78bf4066d4e6c600d7f71dd934db
Nguyen <huunguyen199999@gmail.com>
2025-02-03 17:13:07 +0700

    [Easy] Longest Strictly Increasing or Strictly Decreasing Subarray

--- src/Easy/TwoSum.js
+++ src/Easy/TwoSum.js
@@ -27,7 +27,7 @@ Topics: Array, Hash Table
 
 const twoSum = (nums, target) => {
 	let map = {};
-	for (let i = 0; i <= nums.length; i++) {
+	for (let i = 0; i < nums.length; i++) {
 		let n = nums[i];
 		if (map[target - n] >= 0) {
 			return [map[target - n], i];
